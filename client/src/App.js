69188b4b5c1c4633de2b37db74d22ebb3a40c501
Mark Speer <66832524+mark-speer@users.noreply.github.com>
2020-11-29 20:58:50 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
+
+function App() {
+
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  }
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="App-header">
+      <Header></Header>
+      </div>
+      
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+      </div>
+      <p className="App-intro">{this.state.data}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/yourdata.js
@@ -0,0 +1,56 @@
+export default
+    {
+
+
+        name :'Mark Speer.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! I am Mark Speer.',
+                        //Line 2 For Header
+                        'Web Developer ',
+                        //Line 3 For Header
+                        'From North Carolina'
+    ],
+        //Contact Email
+        contactEmail:'mark.speer11@gmail.com',
+        // Add Your About Text Here
+        abouttext: "I seek to utilize modern coding languages and frameworks to be able to write and deploy sleek, interactive user experiences for the modern online environment. ",
+        aboutImage:'https://i.ibb.co/qnygcgD/headshot.png',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+
+       projects:[
+           {
+            id: 1,
+            title:'Pawsibilities',
+             service:'UI/UX Design',
+             imageSrc:"https://i.ibb.co/FBLw4J6/home-pg-paw.png",
+
+             url:'https://mark-speer.github.io/Pawsibility/'
+            },
+            {
+                id: 2,
+                title: 'Huddle',
+                service: 'Backend Development and Database Administration',
+                imageSrc: "https://i.ibb.co/c34Bpg2/huddleimg.png",
+                url: 'https://project2-team8-dmtv.herokuapp.com/'
+            },
+            { 
+                id: 3,
+                title: 'Project Three',
+                service: 'Day Planner',
+                imageSrc: "https://i.ibb.co/cgh16mP/day-planner.png",
+                url: 'https://mark-speer.github.io/day-planner/'
+            },
+
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/mark-speer'},
+            {
+                name: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/mark-speer/'
+            },
+
+        ]
+    }
